0062b811336fe03defbc9d4ca7f4a82680bd0e98
Neres Gesley <flashitsc@gmail.com>
2022-08-18 23:04:37 -0300

    Initial commit

--- api/v1/src/lib/server.js
+++ api/v1/src/lib/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const server = express()
 const routes = require('../routes/index')
 const handler = require('../utils/handler.js')
+const cors = require('cors')
+
+server.use(cors())
 
 server.use(express.json())
 
