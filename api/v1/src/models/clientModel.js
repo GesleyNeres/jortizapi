678e1af000b1d43cf05c8c2b59587efd9d51aeb0
Neres Gesley <flashitsc@gmail.com>
2022-09-18 21:37:23 -0300

    Initial commit

--- api/v1/src/models/clientModel.js
+++ api/v1/src/models/clientModel.js
@@ -4,51 +4,67 @@ const database = require("../lib/database");
 const crypt = require('../utils/crypt')
 
 const ClientModel = database.define('app_clients',
-    {
-        uuid: {
-            type: Sequelize.UUID,
-            primaryKey: true,
-            defaultValue: Sequelize.UUIDV4
-          },
-          name: {
-            type: Sequelize.STRING,
-            allowNull: false,
-          },
-          email: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            unique:true
-          },
-          phone: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            unique: true
-          },
-          cost:{
-            type: Sequelize.FLOAT,
-            allowNull: false,
-            defaultValue: 0.00
-          },
-          status: {
-            type: Sequelize.BOOLEAN,
-            allowNull: false,
-            defaultValue: false
-          },
-          createdAt: {
-            type: Sequelize.DATE,
-            defaultValue: Date.now(),
-            allowNull: false
-          },
-          updatedAt:{
-            type: Sequelize.DATE,
-            defaultValue: Date.now(),
-            allowNull: false
-          }
+  {
+    uuid: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    phone: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    address:{
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    city:{
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    state:{
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    cost:{
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    total_revenue:{
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    details:{
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    status: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      defaultValue: Date.now(),
+      allowNull: false
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      defaultValue: Date.now(),
+      allowNull: false
     }
+  }
 )
-ClientModel.beforeCreate(function (model) {
-  model.name = crypt.encrypt(mode.name)
-});
-
 
 module.exports = ClientModel
--- migrations/20220815234707-app_clients.js
+++ migrations/20220815234707-app_clients.js
@@ -2,47 +2,67 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('app_clients',
-      {
-        uuid: {
-          type: Sequelize.UUID,
-          primaryKey: true,
-          defaultValue: Sequelize.UUIDV4
-        },
-        name: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        email: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          unique:true
-        },
-        phone: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          unique: true
-        },
-        cost:{
-          type: Sequelize.FLOAT,
-          allowNull: false,
-          defaultValue: 0.00
-        },
-        status: {
-          type: Sequelize.BOOLEAN,
-          allowNull: false,
-          defaultValue: false
-        },
-        createdAt: {
-          type: Sequelize.DATE,
-          defaultValue: Date.now(),
-          allowNull: false
-        },
-        updatedAt:{
-          type: Sequelize.DATE,
-          defaultValue: Date.now(),
-          allowNull: false
-        }
+    {
+      uuid: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      address:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      city:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      state:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cost:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      total_revenue:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      details:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Date.now(),
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Date.now(),
+        allowNull: false
       }
+    }
     );
   },
 
